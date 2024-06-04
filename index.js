8515842577d5369e9c1f583785ee491e27fb5d57
neetak12345 <142806586+neetak12345@users.noreply.github.com>
2024-06-04 15:33:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// first step node ke package ko install kerna 
+// npm i color
+// npm i color-convert
+
+const Color = require('color');
+var convert = require('color-convert');
+
+let c=convert.keyword.rgb('blue');
+const color = Color.rgb(c)
+console.log("RGB" ,color.rgb().string());
+console.log("HSL", color.hsl().string());
+console.log("HEX", color.hex());
+console.log("cymk", color.cmyk().string());
+console.log("HSV", color.hsv().string());
